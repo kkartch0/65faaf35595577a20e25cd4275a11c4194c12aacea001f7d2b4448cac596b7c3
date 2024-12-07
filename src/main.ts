a626ea6d6900b08a9b22364b42dff7024f5b8e0d
Ky Kartchner <kartchnerky@gmail.com>
2024-12-07 14:26:42 +0000

    refactor: rename plugin and update manifest for meal idea generation

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,10 @@ import { Plugin } from "obsidian";
 
 export default class GospelStudyPlugin extends Plugin {
 	/**
-	 * Loads the plugin settings from the data store.
-	 * If no settings are found, the default settings are used.
-	 * @returns A promise that resolves when the settings are loaded.
-	 */
-	public async loadSettings(): Promise<void> {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	/**
 	 * Called when the plugin is loaded.
 	 */
 	public async onload() {
 		console.log("loading the plugin");
-		await this.loadSettings();
 	}
 
 	/**
@@ -27,13 +13,5 @@ export default class GospelStudyPlugin extends Plugin {
 	 */
 	public onunload() {
 		console.log("unloading plugin");
-	}
-
-	/**
-	 * Saves the plugin settings.
-	 * @returns A promise that resolves when the settings are saved.
-	 */
-	public async saveSettings(): Promise<void> {
-		await this.saveData(this.settings);
 	}
 }
